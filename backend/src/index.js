30f521f3a408630a74897e9e054c77138b31f219
harshkr212 <guptagamer8@gmail.com>
2025-06-25 09:28:28 +0530

    Completed authStore and protected the routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 import authRoute from './routes/auth.routes.js'
 import messageRoute from './routes/message.routes.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 dotenv.config();
 const PORT=process.env.PORT || 5002;
@@ -11,6 +12,10 @@ const PORT=process.env.PORT || 5002;
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+   origin:"http://localhost:5173",
+   credentials:true 
+}))
 app.use('/api/auth',authRoute);
 app.use('/api/message',messageRoute);
 connectDB();
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,1 +1,26 @@
-import {create} from 'zustand'
+import { create } from 'zustand'
+import axiosInstance from '../lib/axios.js'
+
+
+ const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigninUp: false,
+    isLoginUp: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get('/auth/checkAuth');
+            set({ authUser: res.data })
+        } catch (error) {
+            console.log("Error in checkAuth", error);
+            set({ authUser: null })
+
+        }
+        finally {
+            set({ isCheckingAuth: false })
+        }
+    }
+
+}))
+export default useAuthStore
