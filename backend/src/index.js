1d19e0aef13d85ac2d7818f90e1f92bc4b3106d2
harshkr212 <guptagamer8@gmail.com>
2025-08-15 10:50:45 +0530

    Added some changes in backend for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://chatapp-mern-3-dtk4.onrender.com",
    credentials:true 
 }))
 
